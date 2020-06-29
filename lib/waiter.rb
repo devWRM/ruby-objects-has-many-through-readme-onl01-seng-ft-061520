4d80cea3aeee7b9c186e58b078745620851819e5
devWRM <mcwen@comcast.net>
2020-06-28 22:49:44 -0400

    Done.

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,39 @@
+require "pry"
+
 class Waiter
+
+    @@all = []
+
+    def initialize(name, years_of_experience)
+        @name = name
+        @years_of_experience = years_of_experience
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.waiter == self
+        end
+    end
+
+    def customers
+        meals.map{|meal| meal.customer}
+    end
+
+    def best_tipper
+        max_meal = meals.max_by do |meal|
+            meal.tip
+        end
+        max_meal.customer
+    end
 
 end
